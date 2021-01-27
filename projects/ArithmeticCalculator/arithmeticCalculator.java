eac24743c8cd260d73a1bf169705aff101e6fd15
KedrickHill <kedrickjhill@gmail.com>
2021-01-27 17:29:00 -0600

    Modified: scanning input w/o errors

--- projects/ArithmeticCalculator/arithmeticCalculator.java
+++ projects/ArithmeticCalculator/arithmeticCalculator.java
@@ -1,12 +1,29 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class arithmeticCalculator {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
+    //  List<String> oper = new ArrayList<>();
+     Scanner in = new Scanner(System.in);
 
-        System.out.println("Enter the Arithmetic Operation:");
-        String operation = input.nextLine();
+     System.out.println("Enter the operation: ");
+     String input = in.nextLine();
 
-        System.out.println("Enter total Values");
+     String[] op = input.split("([+-x/ ])");
+
+     for (String string : op) {
+         System.out.println("The Array: " + string);
+     }
+
+     List<String> operation = Arrays.asList(op);
+     in.close();
+                
+     System.out.println("The List contains: " + operation);
+
     }
 }
