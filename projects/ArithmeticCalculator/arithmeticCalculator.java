9ef7a1ce482175d480feb2be9bac51662a92dda8
KedrickHill <kedrickjhill@gmail.com>
2021-01-27 16:20:01 -0600

    Added: init w/ some scanner work

---
+++ projects/ArithmeticCalculator/arithmeticCalculator.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class arithmeticCalculator {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter the Arithmetic Operation:");
+        String operation = input.nextLine();
+
+        System.out.println("Enter total Values");
+    }
+}
