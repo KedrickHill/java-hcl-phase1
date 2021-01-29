4055e8a81dd18be356cb460d4e6ab5580298e06c
KedrickHill <kedrickjhill@gmail.com>
2021-01-29 16:31:55 -0600

    Added: base input from user (no msg yet)

--- projects/Lesson2/Project/MainApplication.java
+++ projects/Lesson2/Project/MainApplication.java
@@ -1,6 +1,28 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+
 public class MainApplication {
     
     public static void main(String[] args) {
         
+    Scanner input = new Scanner(System.in);
+
+    System.out.println("Who is sending the Email? ");
+    String from = input.nextLine();
+
+    ArrayList<String> to = new ArrayList<String>();
+    
+    String recip = "start";
+
+    while (!recip.equals("end")) {
+        System.out.println("Enter a Recipient (to stop enter 'end'): ");
+        recip = input.nextLine();
+        to.add(recip)
+    }
+     
+
+    System.out.println("From: " + from + "\nTo: " + to );
+
     }
 }
