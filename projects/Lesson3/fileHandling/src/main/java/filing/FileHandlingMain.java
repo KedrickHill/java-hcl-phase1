49dfd629b88340e03c498e5889a02209360c324a
KedrickHill <kedrickjhill@gmail.com>
2021-02-03 13:45:42 -0600

    Init: creation of maven project

---
+++ projects/Lesson3/fileHandling/src/main/java/filing/FileHandlingMain.java
@@ -0,0 +1,13 @@
+package filing;
+
+/**
+ * Hello world!
+ *
+ */
+public class FileHandlingMain 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ projects/Lesson3/fileHandling/src/test/java/filing/AppTest.java
@@ -0,0 +1,20 @@
+package filing;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
